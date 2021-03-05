d49180857d23c807ed6576de15345e888c577ec4
Dat Hoang <dthoang6@gmail.com>
2021-03-04 20:04:03 -0800

    build reviews

---
+++ reviews/app.js
@@ -0,0 +1,27 @@
+//local reviews data
+const reviews = [
+    {
+        id: 1,
+        name: "susan smith",
+        job: "web developer",
+        img: "",
+        text: "It is awsome."
+    },
+    {
+        id: 2,
+        name: "ana john",
+        job: "web designer",
+        img: "",
+        text: "It is a good job."
+    },
+    {
+        id: 3,
+        name: "dr hoang",
+        job: "hacker",
+        img: "",
+        text: "It is good."
+    }
+];
+
+//select items
+const img = document.getElementById("person-img");
