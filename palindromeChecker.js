69692c1a693fb96127ace93350cd784639b39994
Dat Hoang <dthoang6@gmail.com>
2021-07-08 16:36:34 -0700

    using JavaScript to build a Palindrome Checker

---
+++ palindromeChecker.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * @param {*} str
+ * @returns true if the given string is a palindrome, which is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+ */
+function palindrome(str) {
+  return true;
+}
+
+palindrome("eye");
