3ffac59d784689e451770ceb45faa845e8a3f559
Dat Hoang <dthoang6@gmail.com>
2021-07-14 16:22:22 -0700

    using for loop to check if the string if palindrome or not

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -2,6 +2,7 @@
  *
  * @param {*} str
  * @returns true if the given string is a palindrome, which is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+ * @returns false otherwise.
  */
 function palindrome(str) {
   return true;
