b4322acc72020ba4294023724dd1f8eb1e58f31a
Dat Hoang <dthoang6@gmail.com>
2021-09-13 16:24:26 -0700

    update

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -5,6 +5,17 @@
  * @returns false otherwise.
  */
 function palindrome(str) {
+  // find the length of a string
+  const len = str.length;
+
+  // loop through half of the string
+  for (let i = 0; i < len / 2; i++) {
+
+      // check if first and last string are same
+      if (str[i] !== str[len - 1 - i]) {
+          return false;
+      }
+  }
   return true;
 }
 
