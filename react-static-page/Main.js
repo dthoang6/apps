4094fd9a02ef6c36d06fe90d34b84dbd3b04e495
Dat Hoang <dthoang6@gmail.com>
2022-02-23 15:26:50 -0800

    organizing file

---
+++ react-static-page/Main.js
@@ -0,0 +1,13 @@
+export default function MainContent() {
+  return (
+    <div>
+      <h1>Fun Facts about Tom</h1>
+      <ul>
+        <li>is Front-End React Developer.</li>
+        <li>is graduated from Cal Poly Pomona with B.S. Computer Science.</li>
+        <li>like reading coding book.</li>
+        <li>like cooking and travel.</li>
+      </ul>
+    </div>
+  );
+}
